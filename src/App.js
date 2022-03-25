99416094fae1b3d2a978fa85e380e4037c30464d
Ngo Sy Tuan Anh <langthambca@gmail.com>
2022-03-25 10:10:50 +0700

    up lab3.2

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Menu from "./components/MenuComponents";
+import { DISHES } from "./shared/dishes";
+import "./App.css";
+import { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+}
+
+export default App;
